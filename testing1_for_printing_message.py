6e8f07dbacdca7b392e4b5fe6228f16618aa3643
kishor <kishorkaphle216@gmail.com>
2020-11-05 19:16:14 +0545

    updated

--- testing1_for_printing_message.py
+++ testing1_for_printing_message.py
@@ -23,32 +23,12 @@ class RedisWork:
         return self.get_redis().publish(self.channel_name, self.message)
 
 
-class Listener(threading.Thread):
-    def __init__(self, channel_name, message):
-        threading.Thread.__init__(self)
-        self.channel_name = channel_name
-        self.message = message
-        r = RedisWork(self.channel_name, self.message)
-        r.subscribe_channel()
-        
-
-    def get_redis(self):
-        return redis.Redis(host='localhost', port=6379, db=0)   
-
-    def work(self, item):
-        print (item['channel'], ":", item['data'])
-
-    def run(self):
-        for item in self.subscribe_channel().listen():
-            if item['data'] == "KILL":
-                self.pubsub.unsubscribe()
-                print (self, "unsubscribed and finished")
-                break
-            else:
-                self.work(item)
-
-if __name__ == '__main__':
-    client = Listener('channel', 'hello!')
-    client.start()
-
- 
+r = RedisWork('channel', 'hello!')
+r.publish_message()
+data = []
+with r.get_redis().monitor() as m:
+    for command in m.listen():
+        print(command['command'])
+        data.append(command['command'])
+        break
+print(data)
